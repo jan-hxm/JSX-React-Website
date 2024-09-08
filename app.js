98287e41c8507446e4995b0feb6c66e26ad0d05d
Jan <jan.henke01@gmail.com>
2024-09-08 23:54:03 +0200

    Added project structure

---
+++ app.js
@@ -0,0 +1,24 @@
+import { animals } from './animals';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+
+const title = '';
+
+const background = <img class="background" alt="ocean" src="/images/ocean.jpg" />
+
+const images = [];
+for (const animal in animals) {
+  images.push(`<img key={${animal}}src={${animal}}/>`)
+}
+
+const animalFacts = (
+  <div>
+    { background }
+    <h1>{title ? title : 'Click an animal for a fun fact'}</h1>
+  </div>
+  );
+
+root.render(animalFacts);
